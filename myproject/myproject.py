8ec28dac4b80c1ed6cddb9e2d5e5c74ae32ef56d
aliwo <aliwo@naver.com>
2018-08-07 18:18:55 +0900

    이미지 추가

--- myproject/myproject.py
+++ myproject/myproject.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
